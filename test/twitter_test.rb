8733708725be902972a25a3875021b1be38a7924
Kim Daekwon <propellerheaven@gmail.com>
2013-09-17 15:35:50 +0900

    To be deleted

--- test/twitter_test.rb
+++ test/twitter_test.rb
@@ -2,23 +2,11 @@ require File.join(__dir__, '..', 'config', 'boot.rb')
 require "pp"
 
 def test
-t = Pickpocket::Twitter.new
-t.favorites("nacyo_t").each_with_index do |favorites, index|
+
   f = favorites.attrs
 
   if f[:entities][:urls].size > 0
-    puts " ============= " + index.to_s
-    # puts f[:text]
-    puts "name: "
-    puts f[:user][:name]
-    # puts f[:user][:screen_name]
-    # puts f[:created_at]
-    # puts f[:user][:profile_image_url]
-    
-    f[:entities][:urls].each do |url|
-      puts "url: "
-      puts url[:expanded_url]
-    end
+
   end
   end
 rescue # Twitter::Error::TooManyRequests
@@ -30,9 +18,9 @@ def test2
   t = Pickpocket::Twitter.new
   t.timeline("nacyo_t").each do |twe|
     pp twe
+    p
   end
 end
 
 
 test2
-
